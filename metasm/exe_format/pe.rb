d7e33eadd2a431114059b104ba8cc5119aaa9778
jj <none@none>
2007-05-07 18:54:37 +0200

    PE: rewriten

--- metasm/exe_format/pe.rb
+++ metasm/exe_format/pe.rb
@@ -1,153 +1,100 @@
 require 'metasm/exe_format/main'
 require 'metasm/exe_format/mz'
-require 'metasm/exe_format/coff'
+require 'metasm/exe_format/coff_encode'
+require 'metasm/exe_format/coff_decode'
 
 module Metasm
 class PE < ExeFormat
-	class Header
-		attr_accessor :coff_offset
-		def self.decode(pe)
-			hdr = new
-			pe.encoded.ptr = 0x3c
-			hdr.coff_offset = Expression.decode_imm(pe.encoded, :u32, :little) + 4
-			pe.encoded.ptr = hdr.coff_offset - 4
-			raise 'Invalid PE signature' if pe.read(4) != "PE\0\0"
-			hdr
-		end
-	end
+	attr_accessor :coff_offset, :sig
+	attr_accessor :encoded, :mz, :coff
 
 	def self.decode(str)
 		pe = new
-		pe.encoded = pe.coff.encoded = pe.mz.encoded = EncodedData.new << str
+		pe.encoded = EncodedData.new << str
 		pe.decode_header
-		pe.coff.encoded.ptr = pe.header.coff_offset
+		pe.mz.encoded = pe.encoded[0..pe.coff_offset-4]
+		pe.mz.encoded.ptr = 0
+		pe.mz.decode_header
+		pe.coff.encoded = pe.encoded
+		pe.coff.encoded.ptr = pe.coff_offset
 		pe.coff.decode_header
 		pe
 	end
 
-
-	attr_accessor :encoded
-	attr_reader :mz, :coff
-	attr_reader :header
 	def initialize
-		@coff = Coff.new
-		@mz = Mz.new
+		@coff = COFF.new
+		@mz = MZ.new
+		@coff_offset = 0x40
+		@sig = "PE\0\0"
 	end
 
 	def decode_header
-		@header = Header.decode self
+		@encoded.ptr = 0x3c
+		@coff_offset = @encoded.decode_imm(:u32, :little) + 4
+		@encoded.ptr = @coff_offset - 4
+		@sig = @encoded.read(4)
+		raise "Invalid PE signature #{@sig.inspect}" if @sig != "PE\0\0"
 	end
 
-	def encode_mz_header
-		mzstubp = Program.new(Ia32.new(386))
-		mzstubp.parse <<EOMZSTUB
-.mode 16
-.text
-_str	db "Win32 needed\r\n$"
+	def encode_default_mz_header
+		mzstubp = Program.new(Ia32.new(386, 16))
+		mzstubp.parse <<'EOMZSTUB'
+_str	db "Needs Win32!\r\n$"
 start:
 	push cs
 	pop  ds
 	xor  dx, dx	  ; ds:dx = addr of $-terminated string
-	// mov  dx, _str
 	mov  ah, 9
 	int  21h
 	mov  ax, 4c01h    ; exit code in al
 	int  21h
 EOMZSTUB
-
 		mzstubp.encode
-		mzhdr = EncodedData.new
-		mzparts = MZ.pre_encode mzstubp
-		mzhdr << mzparts.shift 
+		@mz = MZ.from_program mzstubp
+		mzparts = @mz.pre_encode
+
+		@mz.encoded = EncodedData.new << mzparts.shift
+		raise 'OH NOES !!1!!!1!' if @mz.encoded.virtsize > 0x3c
+		# put as much as we can before 0x3c
 		until mzparts.empty?
-			break if mzparts.first.virtsize + pehdr.virtsize > 0x3c
-			mzhdr << mzparts.shift
+			break if mzparts.first.virtsize + @mz.encoded.virtsize > 0x3c
+			@mz.encoded << mzparts.shift
 		end
-		mzhdr.fill 0x40
+
+		# set PE signature pointer at 0x3c
+		@mz.encoded.align_size 0x3c
+		@mz.encoded << Expression['pesigptr'].encode(:u32, :little)
+
+		# add the end of the MZ program
 		until mzparts.empty?
-			mzhdr << mzparts.shift
+			@mz.encoded << mzparts.shift
 		end
-		start = mzstubp.label_at mzhdr, 0
-		mzhdr.fixup mzhdr.export.inject({}) { |binding, (name, offset)| binding.update name => Expression[start, :+, offset] }
-		mzhdr.align_size 8
-		mzhdr.data[0x3c, 4] = Expression.encode_immediate(mzhdr.virtsize, :u32, :little)
-		MZ.encode_fix_checksum mzhdr.data
-	end
-end
-end
 
+		# ensure the sig is 8-aligned
+		@mz.encoded.align_size 8
 
-		
-class << self
-	def encode(program, opts={})
-		pehdr = EncodedData.new
-		if opts['pre_header']
-			pehdr << opts.delete('pre_header')
-		else
+		# fixup the MZ program's relocs and the PE sig ptr
+		start = @mz.label_at @mz.encoded, 0
+		@mz.encoded.fixup 'pesigptr' => @mz.encoded.virtsize
+		@mz.encoded.fixup @mz.encoded.export.inject({}) { |binding, (name, offset)| binding.update name => Expression[start, :+, offset] }
 
-		end
-		pehdr.fill 0x40
-		pehdr.data[0x3c, 4] = Expression.encode_immediate(pehdr.virtsize, :u32, program.cpu.endianness)
-		opts['pre_header'] = pehdr.data << "PE\0\0"
-
-		csum = opts['checksum']
-		data = COFF.encode(program, opts)
-		fix_checksum data if not csum
-		data
+		# fixup the MZ checksum
+		@mz.encoded.fill
+		@mz.encode_fix_checksum
 	end
 
-	def fix_checksum(data)
-		# little endian only, wont work with overlapping sections
+	def encode
+		# @mz.encoded must be an EncodedData with 0x3c pointing beyond its last byte, which should be 8-aligned, and its 1st 2 bytes should be 'MZ'
+		encode_default_mz_header if not @mz.encoded
 
-		# check we have a valid PE
-		return if data[0, 2] != 'MZ'
-		off = data[0x3c, 4].unpack('V').first
-		return if data[off, 4] != "PE\0\0"
-		off += 4
+		@encoded = @coff.encoded = @mz.encoded.dup
+		# append the PE signature
+		@encoded << "PE\0\0"
+		# encode the COFF header & body
+		@coff.encoded.ptr = @coff.encoded.virtsize
+		@coff.encode
 
-		# read some header information
-		csumoff = off + 0x14 + 0x40
-		secoff  = off + 0x14 + data[off+0x10, 2].unpack('v').first
-		secnum  = data[off+2, 2].unpack('v').first
-
-		sum = 0
-		flen = 0
-
-		# header
-		# patch csum at 0
-		data[csumoff, 4] = [0].pack('V')
-		curoff  = 0
-		cursize = data[off+0x14+0x3c, 4].unpack('V').first
-		data[curoff, cursize].unpack('v*').each { |s|
-			sum += s
-			sum = (sum & 0xffff) + (sum >> 16) if (sum >> 16) > 0
-		}
-		flen += cursize
-
-		# sections
-		secnum.times { |n|
-			cursize, curoff = data[secoff + 0x28*n + 0x10, 8].unpack('VV')
-			data[curoff, cursize].unpack('v*').each { |s|
-				sum += s
-				sum = (sum & 0xffff) + (sum >> 16) if (sum >> 16) > 0
-			}
-			flen += cursize
-		}
-		sum += flen
-
-		# patch good value
-		data[csumoff, 4] = [sum].pack('V')
+		@encoded.data
 	end
-
-	def is_valid?(data)
-		data[0, 2] == 'MZ' and data[data[0x3c, 4].unpack('V').first, 4] == "PE\0\0"
-	end
-
-	def decode(str, opts)
-	end
-
-	def pre_decode_header(str)
-	end
-end
 end
 end
