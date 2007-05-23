6c0429fe85fa68071df3ddf899f045dd0fd33cf2
jj <none@none>
2007-05-23 18:50:31 +0200

    metasm-shell: allow to return edatas

--- metasm-shell.rb
+++ metasm-shell.rb
@@ -8,32 +8,41 @@ require 'enumerator'
 class String
 	@@cpu = Metasm::Ia32.new
 
-	def encode
+	def encode_edata
 		p = Metasm::Program.new @@cpu
 		p.parse self
 		p.encode
-		ed = p.sections.first.encoded
+		p.sections.first.encoded
+	end
+
+	def encode
+		ed = encode_edata
 		ed.fill
 		ed.data
 	end
 
 	# eip is the address of the entrypoint
 	# base_addr is the address of the first byte of the string
-	def decode(eip=0, base_addr=0)
+	def decode_blocks(eip=0, base_addr=0)
 		p = Metasm::Program.new @@cpu
 		s = Metasm::Section.new p, nil
 		s.encoded << self
 		s.base = base_addr
 		p.sections << s
 		p.desasm eip
+		p.blocks
+	end
+
+	def decode(eip=0, base_addr=0)
 		res = []
 		lastaddr = base_addr
-		p.block.sort.each { |addr, block|
+		blocks = decode_blocks(eip, base_addr)
+		blocks.sort.each { |addr, block|
 			if addr > lastaddr
 				res << s.encoded.data[lastaddr-s.base, addr-lastaddr].unpack('C*').map { |c| '%02xh' % c }.enum_slice(16).map { |e| 'db ' + e.join(', ') + "\n" }.join
 			end
-			if p.block[addr] and not p.block[addr].from.empty?
-				res << "; Xrefs: #{p.block[addr].from.map { |f| '%08X' % f }.join(', ')}"
+			if block[addr] and not block[addr].from.empty?
+				res << "; Xrefs: #{block[addr].from.map { |f| '%08X' % f }.join(', ')}"
 			end
 			s.encoded.export.each { |e, off|
 				res << "#{e}:" if off == addr - s.base and e !~ /^metasmintern/
