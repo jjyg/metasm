bfcec32b9fae10d48f6ff610c332fcce4a5c25bd
jj <none@none>
2007-05-28 19:30:53 +0200

    add '*' repetition marker (a la hexdump)

--- hexdump.rb
+++ hexdump.rb
@@ -2,10 +2,20 @@
 
 require 'enumerator'
 o = -16
+lastl = []
+lastdpl = false
 File.open(ARGV.first, 'rb') { |fd| fd.read }.unpack('C*').each_slice(16) { |s|
-	print '%04x  ' % (o += 16)
-	print s.map { |b| '%02x' % b }.join(' ').ljust(3*16-1) + '  '
-	print s.pack('C*').unpack('L*').map { |bb| '%08x' % bb }.join(' ').ljust(9*4-1) + '  '
-	puts  s.map { |c| (32..126).include?(c) ? c : ?. }.pack('C*')
+	o += 16
+	if s != lastl
+		lastdpl = false
+		print '%04x  ' % o
+		print s.map { |b| '%02x' % b }.join(' ').ljust(3*16-1) + '  '
+		print s.pack('C*').unpack('L*').map { |bb| '%08x' % bb }.join(' ').ljust(9*4-1) + '  '
+		puts  s.map { |c| (32..126).include?(c) ? c : ?. }.pack('C*')
+	elsif not lastdpl
+		lastdpl = true
+		puts '*'
+	end
+	lastl = s
 }
 
