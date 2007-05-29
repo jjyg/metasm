eab836bf0935840ca7462180e54841507b8c5eb0
jj <none@none>
2007-05-29 15:10:44 +0200

    pe patcher: add to last section (avoids overwriting misc data stored in header)

--- testpe-hook.rb
+++ testpe-hook.rb
@@ -5,17 +5,17 @@ require 'metasm-shell'
 
 # code to run on start
 newcode = <<EOS.encode_edata
-hooked_entrypoint:
+hook_entrypoint:
 pushad
-push libname
+push hook_libname
 call [LoadLibraryA]
-push funcname
+push hook_funcname
 push eax
 call [GetProcAddress]
 
 push 0
-push title
-push msg
+push hook_title
+push hook_msg
 push 0
 call eax
 
@@ -23,29 +23,34 @@ popad
 jmp entrypoint
 
 .align 4
-msg db '(c) David Hasselhoff', 0
-title db 'Hooked on a feeling', 0
-libname db 'user32', 0
-funcname db 'MessageBoxA', 0
+hook_msg db '(c) David Hasselhoff', 0
+hook_title db 'Hooked on a feeling', 0
+hook_libname db 'user32', 0
+hook_funcname db 'MessageBoxA', 0
 EOS
 
 # read original file
 target = ARGV.shift
 pe = Metasm::PE.decode Metasm::VirtualFile.read(target)
 
-# add new section
-pe.sections << Metasm::PE::Section.new
-pe.sections.last.name = '.hook'
-pe.sections.last.characteristics = ['MEM_READ', 'MEM_EXECUTE']
-pe.sections.last.encoded = newcode
+# modify last section
+s = pe.sections.last
+s.encoded.data = s.encoded.data.to_str
+s.encoded << newcode
+s.virtaddr = s.virtsize = s.rawaddr = s.rawsize = nil
 
 # patch entrypoint
-pe.optheader.entrypoint = 'hooked_entrypoint'
+pe.optheader.entrypoint = 'hook_entrypoint'
 
 # reencode
+oldhdr = pe.encoded[0, pe.sections.first.rawaddr]
 pe.encoded = Metasm::EncodedData.new
 pe.encode_header
+# bad people store information in unmapped space here (eg import libnames)
+pe.encoded << oldhdr[pe.encoded.virtsize..-1] if oldhdr.virtsize > pe.encoded.virtsize
 pe.encode_sections_fixup
+
+puts "Unresolved relocations: #{pe.encoded.reloc.inspect}" if not pe.encoded.reloc.empty?
 
 # save to file
 File.open(target.sub('.exe', '-patch.exe'), 'wb') { |fd| fd.write pe.encoded.data }
