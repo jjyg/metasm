5dcba743cbc94fbaa232b569091181642e7dd17b
jj <none@none>
2007-05-29 13:55:23 +0200

    dasmtest comments

--- stresstest-dasm.rb
+++ stresstest-dasm.rb
@@ -3,13 +3,22 @@
 require 'metasm-shell'
 
 puts <<EOS.encode(0).decode
+
+; calcule l'adresse du saut
 mov ebx, 0x12345678
 mov eax, ((toto + 12) ^ 0x12345678)
 xor eax, ebx
 sub eax, 12
+
+; saute
 push eax
 ret
+
+; code mort
 nop
+
+; cible du saut
 toto:
 mov eax, 28h
+
 EOS
