57773b27f25b3f8ddb6a08b037026867884f5da7
jj <none@none>
2007-06-18 13:26:02 +0200

    update pe sample

--- samples/testpe-shutup.rb
+++ samples/testpe-shutup.rb
@@ -5,26 +5,26 @@ include Metasm
 pe = PE.assemble Ia32.new, <<EOS
 .section '.text' r w x
 
-.import 'kernel32' 'GetCurrentProcess'
-.import 'advapi32' 'OpenProcessToken'
-.import 'advapi32' 'LookupPrivilegeValueA'
-.import 'advapi32' 'AdjustTokenPrivileges'
-.import 'user32'   'ExitWindowsEx'
+.import 'kernel32' 'GetCurrentProcess' GetCurrentProcess getcurrentprocess
+.import 'advapi32' 'OpenProcessToken'  OpenProcessToken openprocesstoken
+.import 'advapi32' 'LookupPrivilegeValueA' LookipPrivilegeValueA lookupprivilegevalue
+.import 'advapi32' 'AdjustTokenPrivileges' AdjustTokenPrivileges adjusttokenprivileges
+.import 'user32'   'ExitWindowsEx' ExitWindowsEx exitwindowsex
 
 .entrypoint
 
 ; OpenProcessToken(GetCurrentProcess, ADJUST_PRIV | QUERY, &htok)
 push htok
 push 28h
-call [GetCurrentProcess]
+call getcurrentprocess
 push eax
-call [OpenProcessToken]
+call openprocesstoken
 
 ; LookupPrivVal(0, SE_SHUTDOWN, &tokpriv.priv[0].luid)
 push tokpriv_luid
 push privname
 push 0
-call [LookupPrivilegeValueA]
+call lookupprivilegevalue
 
 ; tokpriv.privcnt = 1 ; tokpriv.priv[0].attr = ENABLED
 mov dword ptr [tokpriv], 1
@@ -38,12 +38,12 @@ push eax
 push tokpriv
 push eax
 push dword ptr [htok]
-call [AdjustTokenPrivileges]
+call adjusttokenprivileges
 
 ; ExitWindowsEx(SHUTDOWN | FORCE, OS | MINORUPDATE | PLANNED)
 push 80020003h
 push 5
-call [ExitWindowsEx]
+call exitwindowsex
 
 ret
 
