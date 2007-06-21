6f12570a0d127ee2c5d39595c518e9b6fafd10ac
jj <none@none>
2007-06-21 17:44:51 +0200

    pe sample: add comments

--- samples/pe-hook.rb
+++ samples/pe-hook.rb
@@ -5,6 +5,10 @@
 #    Licence is LGPL, see LICENCE in the top-level directory
 
 
+# 
+# in this file, we open an existing PE, add some code to its last section and
+# patch the entrypoint so that we are executed at program start
+#
 
 require 'metasm'
 require 'metasm-shell'
@@ -36,14 +40,14 @@ hook_funcname db 'MessageBoxA', 0
 EOS
 
 # read original file
-target = ARGV.shift
+raise 'need a target filename' if not target = ARGV.shift
 pe = Metasm::PE.decode_file(target)
 
 # modify last section
 s = pe.sections.last
-s.encoded.data = s.encoded.data.to_str
+s.encoded.data = s.encoded.data.to_str	# get rid of the VirtualString, if any
 s.encoded << newcode
-s.virtaddr = s.virtsize = s.rawaddr = s.rawsize = nil
+s.virtaddr = s.virtsize = s.rawaddr = s.rawsize = nil	# reset those value, so that the linker computes them for us
 s.encoded.fixup!('entrypoint' => pe.optheader.image_base + pe.optheader.entrypoint)
 
 # patch entrypoint
@@ -53,7 +57,7 @@ pe.optheader.entrypoint = 'hook_entrypoint'
 oldhdr = pe.encoded[0, pe.sections.first.rawaddr]
 pe.encoded = Metasm::EncodedData.new
 pe.encode_header
-# bad people store information in unmapped space here (eg import libnames)
+# bad people store information in unmapped space here (eg import libnames), so we try to keep the header as similar as possible to what it was
 pe.encoded << oldhdr[pe.encoded.virtsize..-1] if oldhdr.virtsize > pe.encoded.virtsize
 pe.encode_sections_fixup
 
