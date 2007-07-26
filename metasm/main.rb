c0d03206a151911be7751e014db019329aa3d05c
jj <none@none>
2007-07-26 15:00:34 +0200

    bugfix

--- metasm/main.rb
+++ metasm/main.rb
@@ -186,7 +186,7 @@ class Token
 	def dup
 		n = self.class.new(backtrace)
 		n.type = @type
-		n.value = @value.respond_to?(:dup) ? @value.dup : @value
+		n.value = @value.kind_of?(String) ? @value.dup : @value
 		n.raw = @raw.dup
 		n
 	end
