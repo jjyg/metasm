e3164475ac5b665141ab68b8a58a577ce29e4557
jj <none@none>
2008-12-16 16:58:22 +0100

    sample/exesamples

--- samples/exeencode.rb
+++ samples/exeencode.rb
@@ -5,17 +5,17 @@
 #    Licence is LGPL, see LICENCE in the top-level directory
 
 #
-# this sample shows how to compile an ELF file
-# use --exe PE to compile a PE
-# use --cpu MIPS, --16, --be to change the CPU
-# either from C or ASM source
+# this sample shows how to compile an executable file from source
+# use --exe PE to compile a PE/ELF/MachO etc
+# use --cpu MIPS/--16/--be to change the CPU
+# the arg is a source file (c or asm) (some arch may not yet support C compiling)
 #
 
 require 'metasm'
 require 'optparse'
 
-execlass ||= Metasm::ELF
-cpu ||= Metasm::Ia32.new
+$execlass ||= Metasm::ELF
+$cpu ||= Metasm::Ia32.new
 
 outfilename = 'a.out'
 type = nil
@@ -25,12 +25,12 @@ OptionParser.new { |opt|
 	opt.on('--asm') { type = 'asm' }
 	opt.on('-v', '-W') { $VERBOSE=true }
 	opt.on('-d') { $DEBUG=$VERBOSE=true }
-	opt.on('-e class', '--exe class') { |c| execlass = Metasm.const_get(c) }
-	opt.on('--cpu cpu') { |c| cpu = Metasm.const_get(c).new }
+	opt.on('-e class', '--exe class') { |c| $execlass = Metasm.const_get(c) }
+	opt.on('--cpu cpu') { |c| $cpu = Metasm.const_get(c).new }
 	# must come after --cpu in commandline
-	opt.on('--16') { cpu.size = 16 }
-	opt.on('--le') { cpu.endianness = :little }
-	opt.on('--be') { cpu.endianness = :big }
+	opt.on('--16') { $cpu.size = 16 }
+	opt.on('--le') { $cpu.endianness = :little }
+	opt.on('--be') { $cpu.endianness = :big }
 }.parse!
 
 if file = ARGV.shift
@@ -41,40 +41,21 @@ else
 end
 
 if type == 'c'
-	elf = execlass.compile_c(cpu, src)
+	exe = $execlass.compile_c($cpu, src)
 else
-	elf = execlass.assemble(cpu, src)
+	exe = $execlass.assemble($cpu, src)
 end
-elf.encode_file(outfilename)
+exe.encode_file(outfilename)
 
 __END__
-.interp '/lib/ld-linux.so.2'
-.pt_gnu_stack rw
-
-.data
-toto db "world", 0
-fmt db "Hello, %s !\n", 0
-
 .text
 .entrypoint
- call metasm_intern_geteip
- mov esi, eax
- lea eax, [esi-metasm_intern_geteip+toto]
- push eax
- lea eax, [esi-metasm_intern_geteip+fmt]
- push eax
- call printf
- add esp, 8
+push bla
+push fmt
+call printf
+push 0
+call exit
 
- push 28h
- call _exit
- add esp, 4
- ret
-
-metasm_intern_geteip:
- call 1f
-1:
- pop eax
- add eax, metasm_intern_geteip - 1b
- ret
-
+.data
+bla db "world", 0
+fmt db "Hello, %s !\n", 0
