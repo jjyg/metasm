1f82d23fa58cdfa14ac8936479477cbc50b2c08e
jj <none@none>
2009-01-13 12:09:05 +0100

    misc/hexdump: handle big files, specify format, etc

--- misc/hexdump.rb
+++ misc/hexdump.rb
@@ -8,29 +8,45 @@
 
 require 'enumerator'
 
+class IO
+def hexdump(fmt=nil)
+	ctx = {:noend => true}
+	while buf = read(512) and not buf.empty?
+		buf.hexdump(fmt, ctx)
+	end
+	ctx.delete :noend
+	''.hexdump(fmt, ctx)
+end
+end
+
 class String
-def hexdump
-	o = -16
-	lastl = []
-	lastdpl = false
+def hexdump(fmt=nil, ctx={})
+	fmt ||= ['c', 'd', 'a']
+	ctx[:pos] ||= 0
+	ctx[:lastline] ||= []
+	ctx[:lastdup]
 	unpack('C*').each_slice(16) { |s|
-		o += 16
-		if s != lastl
-			lastdpl = false
-			print '%04x  ' % o
-			print s.map { |b| '%02x' % b }.join(' ').ljust(3*16-1) + '  '
-			print s.pack('C*').unpack('L*').map { |bb| '%08x' % bb }.join(' ').ljust(9*4-1) + '  '
-			puts  s.map { |c| (32..126).include?(c) ? c : ?. }.pack('C*')
-		elsif not lastdpl
-			lastdpl = true
+		if s != ctx[:lastline]
+			ctx[:lastdup] = false
+			print '%04x  ' % ctx[:pos]
+			print s.map { |b| '%02x' % b }.join(' ').ljust(3*16-1) + '  ' if fmt.include? 'c'
+			print s.pack('C*').unpack('L*').map { |bb| '%08x' % bb }.join(' ').ljust(9*4-1) + '  ' if fmt.include? 'd'
+			print s.map { |c| (32..126).include?(c) ? c : ?. }.pack('C*') if fmt.include? 'a'
+			puts
+		elsif not ctx[:lastdup]
+			ctx[:lastdup] = true
 			puts '*'
 		end
-		lastl = s
+		ctx[:lastline] = s
+		ctx[:pos] += s.length
 	}
-	puts '%04x' % length
+	puts '%04x' % ctx[:pos] if not ctx[:noend]
 end
 end
 
 if $0 == __FILE__
-	File.open(ARGV.first, 'rb') { |fd| fd.read }.hexdump
+	(fmt ||= [] << 'c' << 'a') if ARGV.delete '-C'
+	(fmt ||= [] << 'd' << 'a') if ARGV.delete '-d'
+	(fmt ||= [] << 'c' << 'd' << 'a') if ARGV.delete '-a'
+	File.open(ARGV.first, 'rb').hexdump(fmt)
 end
