65a9a747e77cf0510149e64359c8388e75c6a6ca
jj <none@none>
2009-03-05 11:30:43 +0100

    require "metasm" -> autorequire, add metasm/<cpu>.rb

--- metasm.rb
+++ metasm.rb
@@ -4,33 +4,57 @@
 #    Licence is LGPL, see LICENCE in the top-level directory
 
 
-# temporarily put the current file directory in the ruby include path
-incdir = Metasmdir = File.dirname(__FILE__)
-if $:.include? incdir
-	incdir = nil
-else
-	$: << incdir
+Metasmdir = File.dirname(__FILE__)
+
+module Metasm
+
+def const_missing(c) Metasm.const_missing(c) end
+def self.const_missing(c)
+	# constant defined in the same file as another
+	cst = {
+		'X86' => 'Ia32', 'PPC' => 'PowerPC',
+		'UniversalBinary' => 'MachO', 'COFFArchive' => 'COFF',
+		'PTrace32' => 'LinOS', 'GNUExports' => 'LinOS',
+		'LinuxRemoteString' => 'LinOS',
+		'WinAPI' => 'WinOS', 'WindowsExports' => 'WinOS',
+		'WindowsRemoteString' => 'WinOS', 'WinDbg' => 'WinOS',
+		'VirtualFile' => 'OS', 'VirtualString' => 'OS',
+		'EncodedData' => 'Expression',
+	}[c.to_s] || c.to_s
+
+	files = {
+		'Ia32' => 'ia32', 'MIPS' => 'mips', 'PowerPC' => 'ppc',
+		'C' => ['parse_c', 'compile_c'],
+		'MZ' => 'exe_format/mz', 'PE' => 'exe_format/pe',
+		'ELF' => ['exe_format/elf_encode', 'exe_format/elf_decode'],
+		'COFF' => ['exe_format/coff_encode', 'exe_format/coff_decode'],
+		'Shellcode' => 'exe_format/shellcode', 'AutoExe' => 'exe_format/autoexe',
+		'AOut' => 'exe_format/a_out', 'MachO' => 'exe_format/macho',
+		'NDS' => 'exe_format/nds', 'XCoff' => 'exe_format/xcoff',
+		'GtkGui' => 'gui/gtk',
+		'OS' => 'os/main',
+		'LinOS' => 'os/linux', 'WinOS' => 'os/windows',
+		'Disassembler' => 'decode', 'Expression' => ['main', 'encode', 'decode'],
+	}[cst]
+
+	return super if not files	# XXX does it work if another module included defines const_missing?
+
+	files = [files] if files.kind_of? ::String
+
+	# temporarily put the current file directory in the ruby include path
+	if not $:.include? Metasmdir
+		incdir = Metasmdir
+		$: << incdir
+	end
+	files.each { |f| require File.join('metasm', f) }
+	$:.delete incdir if incdir
+
+	const_get c
 end
 
-# cpu architectures
-%w[ia32 mips ppc].each { |f|
-	require "metasm/#{f}/render" if File.exist? File.join(Metasmdir, 'metasm', f, 'render.rb')
-	require "metasm/#{f}/parse"
-	require "metasm/#{f}/encode"
-	require "metasm/#{f}/decode"
-	require "metasm/#{f}/compile_c" if File.exist? File.join(Metasmdir, 'metasm', f, 'compile_c.rb')
-}
-# executable formats
-%w[mz elf_encode elf_decode pe coff_encode coff_decode shellcode a_out xcoff nds autoexe macho].each { |f|
-	require "metasm/exe_format/#{f}"
-}
-# os-specific features
-%w[windows linux].each { |f|
-	require "metasm/os/#{f}"
-}
-
-require 'metasm/parse_c'
-require 'metasm/compile_c'
-
-# cleanup include path
-$:.delete incdir if incdir
+# without this, include Metasm => const_missing magick doesn't work anymore
+# this will probably break x.const_missing..
+def self.included(x)
+	x.class_eval 'def self.const_missing(c) Metasm.const_missing(c) end'
+end
+end
