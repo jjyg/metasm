e9189d70093b7d5460918e91ecd2699890b6f0a7
jj <none@none>
2009-04-27 17:33:28 +0200

    ia32/decode: fix code_binding truncating 32bit regs in 16bit mode

--- metasm/ia32/decode.rb
+++ metasm/ia32/decode.rb
@@ -1033,7 +1033,7 @@ class Ia32
 				else bt[finish, reg, false]
 				end
 			next if val == Expression[reg]
-			val = Expression[val, :&, (1<<@size)-1].reduce
+			val = Expression[val, :&, 0xffff_ffff].reduce #( 1<<@size)-1  # 16bit code may use e.g. edi
 			binding[reg] = Expression[val]
 		}
 
