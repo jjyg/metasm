5e974eab4416ad028dcabcbb8641a41d9413a697
jj <none@none>
2009-05-14 16:30:21 +0200

    ia32/compile: cleaner test for c vars used in inline asm (fails as unsupported)

--- metasm/ia32/compile_c.rb
+++ metasm/ia32/compile_c.rb
@@ -1198,7 +1198,7 @@ class CCompiler < C::Compiler
 		if stmt.output or stmt.input or stmt.clobber
 			raise # TODO (handle %%0 => eax, gas, etc)
 		else
-			raise if @state.func.initializer.symbol.keys.find { |sym| stmt.body.include? sym }	# gsub ebp+off ?
+			raise if @state.func.initializer.symbol.keys.find { |sym| stmt.body =~ /\b#{Regexp.escape(sym)}\b/ }	# gsub ebp+off ?
 			@source << stmt.body
 		end
 	end
