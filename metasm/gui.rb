a77fdac9242cc24e767a93abc5e7b972f1faf9fe
jj <none@none>
2009-11-19 18:40:02 +0100

    gui/qt: 1st try

--- metasm/gui.rb
+++ metasm/gui.rb
@@ -1,1 +1,18 @@
-require 'metasm/gui/gtk'
+backend = case ENV['METASM_GUI']
+when 'gtk'; 'gtk'
+when 'qt'; 'qt'
+else
+	puts "Unsupported METASM_GUI #{ENV['METASM_GUI'].inspect}" if $VERBOSE and ENV['METASM_GUI']
+	begin
+		require 'gtk2'
+		'gtk'
+	rescue LoadError
+		begin
+			require 'Qt4'
+			'qt'
+		rescue LoadError
+			raise LoadError, 'No GUI ruby binding installed - please install libgtk2-ruby or libqt4-ruby'
+		end
+	end
+end
+require "metasm/gui/#{backend}"
