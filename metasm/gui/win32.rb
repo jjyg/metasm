5c8d4e2c33cc9d5ae221a48e72032f2e8333d916
jj <none@none>
2010-02-05 01:05:17 +0100

    gui/win32: msgbox constants

--- metasm/gui/win32.rb
+++ metasm/gui/win32.rb
@@ -516,6 +516,39 @@ typedef struct tagACCEL {
 #define IDI_ASTERISK        32516
 #define IDI_WINLOGO         32517
 
+#define MB_OK                       0x00000000L
+#define MB_OKCANCEL                 0x00000001L
+#define MB_ABORTRETRYIGNORE         0x00000002L
+#define MB_YESNOCANCEL              0x00000003L
+#define MB_YESNO                    0x00000004L
+#define MB_RETRYCANCEL              0x00000005L
+#define MB_CANCELTRYCONTINUE        0x00000006L
+#define MB_ICONHAND                 0x00000010L
+#define MB_ICONQUESTION             0x00000020L
+#define MB_ICONEXCLAMATION          0x00000030L
+#define MB_ICONASTERISK             0x00000040L
+#define MB_USERICON                 0x00000080L
+#define MB_ICONWARNING              MB_ICONEXCLAMATION
+#define MB_ICONERROR                MB_ICONHAND
+#define MB_ICONINFORMATION          MB_ICONASTERISK
+#define MB_ICONSTOP                 MB_ICONHAND
+#define MB_DEFBUTTON1               0x00000000L
+#define MB_DEFBUTTON2               0x00000100L
+#define MB_DEFBUTTON3               0x00000200L
+#define MB_DEFBUTTON4               0x00000300L
+#define MB_APPLMODAL                0x00000000L
+#define MB_SYSTEMMODAL              0x00001000L
+#define MB_TASKMODAL                0x00002000L
+#define MB_HELP                     0x00004000L
+#define MB_NOFOCUS                  0x00008000L
+#define MB_SETFOREGROUND            0x00010000L
+#define MB_DEFAULT_DESKTOP_ONLY     0x00020000L
+#define MB_TOPMOST                  0x00040000L
+#define MB_RIGHT                    0x00080000L
+#define MB_RTLREADING               0x00100000L
+#define MB_SERVICE_NOTIFICATION          0x00200000L
+#define MB_SERVICE_NOTIFICATION_NT3X     0x00040000L
+
 #define IDOK                1
 #define IDCANCEL            2
 #define IDABORT             3
@@ -757,6 +790,11 @@ BOOL
 WINAPI
 BringWindowToTop(
     __in HWND hWnd);
+WINUSERAPI
+BOOL
+WINAPI
+SetForegroundWindow(
+	__in HWND hWnd);
 WINUSERAPI
 BOOL
 WINAPI
