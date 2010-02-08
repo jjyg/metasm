2b4886a9fb2ac0a13ffcf44bc84e0e093f7534a7
jj <none@none>
2010-02-08 13:16:55 +0100

    gui/win32: bgcolor

--- metasm/gui/win32.rb
+++ metasm/gui/win32.rb
@@ -868,6 +868,46 @@ typedef struct tagACCEL {
 #define CF_LOCALE           16
 #define CF_DIBV5            17
 
+#define COLOR_SCROLLBAR         0
+#define COLOR_BACKGROUND        1
+#define COLOR_ACTIVECAPTION     2
+#define COLOR_INACTIVECAPTION   3
+#define COLOR_MENU              4
+#define COLOR_WINDOW            5
+#define COLOR_WINDOWFRAME       6
+#define COLOR_MENUTEXT          7
+#define COLOR_WINDOWTEXT        8
+#define COLOR_CAPTIONTEXT       9
+#define COLOR_ACTIVEBORDER      10
+#define COLOR_INACTIVEBORDER    11
+#define COLOR_APPWORKSPACE      12
+#define COLOR_HIGHLIGHT         13
+#define COLOR_HIGHLIGHTTEXT     14
+#define COLOR_BTNFACE           15
+#define COLOR_BTNSHADOW         16
+#define COLOR_GRAYTEXT          17
+#define COLOR_BTNTEXT           18
+#define COLOR_INACTIVECAPTIONTEXT 19
+#define COLOR_BTNHIGHLIGHT      20
+#define COLOR_3DDKSHADOW        21
+#define COLOR_3DLIGHT           22
+#define COLOR_INFOTEXT          23
+#define COLOR_INFOBK            24
+#define COLOR_HOTLIGHT          26
+#define COLOR_GRADIENTACTIVECAPTION 27
+#define COLOR_GRADIENTINACTIVECAPTION 28
+#define COLOR_MENUHILIGHT       29
+#define COLOR_MENUBAR           30
+#define COLOR_DESKTOP           COLOR_BACKGROUND
+#define COLOR_3DFACE            COLOR_BTNFACE
+#define COLOR_3DSHADOW          COLOR_BTNSHADOW
+#define COLOR_3DHIGHLIGHT       COLOR_BTNHIGHLIGHT
+#define COLOR_3DHILIGHT         COLOR_BTNHIGHLIGHT
+#define COLOR_BTNHILIGHT        COLOR_BTNHIGHLIGHT
+
+
+WINUSERAPI DWORD WINAPI GetSysColor(__in int nIndex);
+
 
 WINUSERAPI
 int
@@ -1801,7 +1841,7 @@ puts "init #{self.class}", caller if self.class.name =~ /AsmList/
 		}.each { |tag, val|
 			@color[tag] = color(val)
 		}
-		@color[:winbg] = 0x6d6969
+		@color[:winbg] = Win32Gui.getsyscolor(Win32Gui::COLOR_BTNFACE)
 		set_color_association(@default_color_association)	# should be called after Gui.main
 		set_font('courier 10')
 		initialize_visible if respond_to? :initialize_visible
@@ -1993,8 +2033,9 @@ end
 			else
 				Win32Gui.selectobject(hdc, Win32Gui.getstockobject(Win32Gui::DC_BRUSH))
 				Win32Gui.selectobject(hdc, Win32Gui.getstockobject(Win32Gui::DC_PEN))
-				Win32Gui.setdcbrushcolor(hdc, 0x6d6969)
-				Win32Gui.setdcpencolor(hdc, 0x6d6969)
+				col = Win32Gui.getsyscolor(Win32Gui::COLOR_BTNFACE)
+				Win32Gui.setdcbrushcolor(hdc, col)
+				Win32Gui.setdcpencolor(hdc, col)
 				Win32Gui.rectangle(hdc, 0, 0, @width, @height)
 			end
 			Win32Gui.endpaint(hwnd, ps)
