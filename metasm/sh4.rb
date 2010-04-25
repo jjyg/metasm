0a8111fc3554bea0dc209eff77e3897a124c6ac4
jj <none@none>
2010-04-25 16:48:00 +0200

    sh4: add license header

--- metasm/sh4.rb
+++ metasm/sh4.rb
@@ -1,2 +1,8 @@
-require 'metasm/sh4/opcodes'
+#    This file is part of Metasm, the Ruby assembly manipulation suite
+#    Copyright (C) 2006-2009 Yoann GUILLOT
+#
+#    Licence is LGPL, see LICENCE in the top-level directory
+
+
+require 'metasm/main'
 require 'metasm/sh4/decode'
