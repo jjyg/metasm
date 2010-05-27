dad0dbbb6e2769b63aba189953ccfcdcceb78ec0
jj <none@none>
2010-05-27 15:34:57 +0200

    gui/x11 bla

--- metasm/gui/x11.rb
+++ metasm/gui/x11.rb
@@ -8,26 +8,21 @@ require 'metasm/dynldr'
 module Metasm
 module Gui
 class XGui < DynLdr
-	new_api_c '#include <X11/Xlib.h>', 'libX11.so'
+	new_api_c <<EOS, 'libX11.so'
+#pragma prepare_gcc
+#include <X11/Xlib.h>
+EOS
 
 def self.test
-p 1
 	d = xopendisplay(nil)
-p 2
 	s = xdefaultscreen(d)
-p 3
+	cmap = xdefaultcolormap(d, s)
 	w = xcreatesimplewindow(d, xdefaultrootwindow(d), 0, 0, 28, 28, 0, xblackpixel(d, s), xwhitepixel(d, s))
-p 4
-	xselectinput(d, w, EXPOSUREMASK|KEYPRESSMASK|BUTTONPRESSMASK)
-p 5
-	xmapwindow(d, w)
-p 6
-	map = xdefaultcolormap(d, s)
-p 7
+	xstorename(d, w, "lol")
 	gc = xcreategc(d, w, 0, 0)
-	#xalloccolor
-	#xflush(d)
-p 8
+	xsetforeground(d, gc, xwhitepixel(d, s))
+	xselectinput(d, w, EXPOSUREMASK|KEYPRESSMASK|BUTTONPRESSMASK)
+	xmapwindow(d, w)
 	msg = alloc_c_struct('XEvent')
 str = 'lolz'
 x = 12
@@ -35,30 +30,27 @@ y = 20
 w = 50
 h = 30
 	loop {
-p d, msg
-p 9
+p :loop
 		xnextevent(d, msg)
 		case msg['type']
 		when EXPOSE
-p 10
+p :expose
 			#xsetforeground(d, gc, col)
-			xdrawrectangle(d, w, gc, x, y, w, h)
-p 11
-			xfillrectangle(d, w, gc, x, y, w, h)
-p 12
+			#xdrawrectangle(d, w, gc, x, y, w, h)
+			#xfillrectangle(d, w, gc, x, y, w, h)
 			#xdrawline(d, w, gc, todo)
-p 13
 			xdrawstring(d, w, gc, x, y, str, str.length)
 		when KEYPRESS
-p 14
+p :keypress
 			k = xlookupkeysym(msg['key'], 0)
+			p k
+		when BUTTONPRESS
+p :buttonpress
+			break
 		end
 	}
-p 15
 	xdestroywindow(d, w)
-p 16
 	xclosedisplay(d)
-p 17
 end
 
 test
