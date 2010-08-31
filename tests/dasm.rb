9210b60612f2054ad11ed9fe181939840b22d1e0
jj <none@none>
2010-08-31 12:31:29 +0200

    tests/dasm: rewrite&add compose_bt_bind tests

--- tests/dasm.rb
+++ tests/dasm.rb
@@ -8,35 +8,32 @@ require 'test/unit'
 require 'metasm'
 
 class TestPreproc < Test::Unit::TestCase
+	include Metasm
+
 	def asm_dasm(src)
-		@cpu ||= Metasm::Ia32.new
-		raw = Metasm::Shellcode.assemble(src, @cpu).encode_string
-		dasm = Metasm::Shellcode.decode(raw, @cpu).disassembler
+		@cpu ||= Ia32.new
+		raw = Shellcode.assemble(src, @cpu).encode_string
+		dasm = Shellcode.decode(raw, @cpu).disassembler
 		dasm.disassemble_fast(0)
 		dasm
 	end
 
+	def do_test_bd2(src, bd)
+		d = asm_dasm(src)
+		calc_bd = d.compose_bt_binding(*d.decoded[0].block.list)
+		calc_bd.delete_if { |k, v| k.to_s =~ /flag/ }
+		assert_equal bd, calc_bd
+	end
+
 	def test_compose_bt_binding
-		d = asm_dasm <<EOS
-mov eax, 1
-mov ebx, 2
-EOS
-		di0, di1 = d.decoded[0].block.list
-		assert_equal({:eax => Metasm::Expression[1], :ebx => Metasm::Expression[2]}, d.compose_bt_binding(di0, di1))
-
-		d = asm_dasm <<EOS
-mov eax, 1
-push eax
-EOS
-		di0, di1 = d.decoded[0].block.list
-		assert_equal({:eax => Metasm::Expression[1], Metasm::Indirection[:esp, 4] => Metasm::Expression[1], :esp => Metasm::Expression[:esp, :+, -4]}, d.compose_bt_binding(di0, di1))
-
-		d = asm_dasm <<EOS
-push 1
-push 2
-EOS
-		di0, di1 = d.decoded[0].block.list
-		assert_equal({:esp => Metasm::Expression[:esp, :+, -8], Metasm::Indirection[:esp, 4] => Metasm::Expression[2], Metasm::Indirection[[:esp, :+, 4], 4] => Metasm::Expression[1] }, d.compose_bt_binding(di0, di1))
+		do_test_bd2 'mov eax, 1  mov ebx, 2', :eax => Expression[1], :ebx => Expression[2]
+		do_test_bd2 'mov eax, 1  push eax', :eax => Expression[1], Indirection[:esp, 4] => Expression[1], :esp => Expression[:esp, :+, -4]
+		do_test_bd2 'mov [eax], ebx  mov [eax], ecx', Indirection[:eax, 4] => Expression[:ecx]
+		do_test_bd2 'add eax, 4  mov [eax], ecx', Indirection[:eax, 4] => Expression[:ecx], :eax => Expression[:eax, :+, 4]
+		do_test_bd2 'mov [eax], ecx  mov ebx, eax', :ebx => Expression[:eax], Indirection[:eax, 4] => Expression[:ecx], Indirection[:ebx, 4] => Expression[:ecx]
+		do_test_bd2 'mov [eax], ecx  add eax, 4', :eax => Expression[:eax, :+, 4], Indirection[[:eax, :+, -4], 4] => Expression[:ecx]
+		do_test_bd2 'mov [eax+4], ecx  add eax, 4', :eax => Expression[:eax, :+, 4], Indirection[:eax, 4] => Expression[:ecx]
+		do_test_bd2 'push 1  push 2', :esp => Expression[:esp, :+, -8], Indirection[:esp, 4] => Expression[2], Indirection[[:esp, :+, 4], 4] => Expression[1]
 	end
 end
 
