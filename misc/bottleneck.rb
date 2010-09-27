fabff4e8080eb5256ec7588ca5e90319f1f1897f
jj <none@none>
2010-09-27 10:52:29 +0200

    misc/bottleneck: allow singleton methods, misc update

--- misc/bottleneck.rb
+++ misc/bottleneck.rb
@@ -13,25 +13,39 @@
 # now you know what to rewrite
 
 
-def log_caller(cls, meth, histlen=-1)
-	malias = meth.to_s.gsub(/[^a-z0-9_]/i, '') + '_log_caller'
-	mcntr = '$' + meth.to_s.gsub(/[^a-z0-9_]/i, '') + '_counter'
+def log_caller(cls, meth, singleton=false, histlen=-1)
+	dec_meth = 'm_' + meth.to_s.gsub(/[^\w]/) { |c| c.unpack('H*')[0] }
+	malias = dec_meth + '_log_caller'
+	mcntr = '$' + dec_meth + '_counter'
 	eval <<EOS
-class #{cls}
+#{cls.kind_of?(Class) ? 'class' : 'module'} #{cls}
+#{'class << self' if singleton}
  alias #{malias} #{meth}
  def #{meth}(*a, &b)
   #{mcntr}[caller[0..#{histlen}]] += 1
   #{malias}(*a, &b)
  end
+#{'end' if singleton}
 end
 
 #{mcntr} = Hash.new(0)
-at_exit { puts " callers of #{cls} #{meth}:", #{mcntr}.sort_by { |k, v| -v }[0, 4].map { |k, v| ["\#{v} times from", k, ''] } }
+at_exit {
+	puts " callers of #{cls} #{meth}:",
+		#{mcntr}.sort_by { |k, v|
+			-v
+		}[0, 4].map { |k, v|
+			["\#{v} times from", k, '']
+		}
+}
 EOS
 end
 
-if ENV['LOGCALLER'] =~ /^(.*)#(.*)$/
-	cls, meth = $1, $2.to_sym
-	cls = cls.split('::').inject(Object) { |o, cst| o.const_get(cst) }
-	log_caller(cls, meth)
+if ENV['LOGCALLER']
+	ENV['LOGCALLER'].split(';').map { |lc|
+		next if not lc =~ /^(.*)([.#])(.*)$/
+		cls, sg, meth = $1, $2, $3.to_sym
+		sg = { '.' => true, '#' => false }[sg]
+		cls = cls.split('::').inject(::Object) { |o, cst| o.const_get(cst) }
+		log_caller(cls, meth, sg)
+	}
 end
