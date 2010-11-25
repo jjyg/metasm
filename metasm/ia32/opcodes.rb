6ac25602180fcefe0e30f781a1199af6a0c92e04
jj <none@none>
2010-11-25 02:33:25 +0100

    ia32: out 84h, al

--- metasm/ia32/opcodes.rb
+++ metasm/ia32/opcodes.rb
@@ -174,6 +174,7 @@ class Ia32
 		addop('mov',   [0x0F, 0x20, 0xC0], :reg, {:d => [1, 1], :eeec => [2, 3]}, :eeec) { |op| op.args.reverse! }
 		addop('mov',   [0x0F, 0x21, 0xC0], :reg, {:d => [1, 1], :eeed => [2, 3]}, :eeed) { |op| op.args.reverse! }
 		addop('mov',   [0x8C], 0,    {:d => [0, 1], :seg3 => [1, 3]}, :seg3) { |op| op.args.reverse! }
+		addop 'out',   [0xE6], nil,  {:w => [0, 0]}, :u8, :reg_eax
 		addop 'out',   [0xE6], nil,  {:w => [0, 0]}, :reg_eax, :u8
 		addop 'out',   [0xE6], nil,  {:w => [0, 0]}, :u8
 		addop 'out',   [0xEE], nil,  {:w => [0, 0]}, :reg_dx, :reg_eax
