f9b3a70e6fb800111e47c319cdacefd90bc3ef83
jj <none@none>
2011-02-24 14:42:51 +0100

    ia32: avoid useless opsz prefixes

--- metasm/ia32/encode.rb
+++ metasm/ia32/encode.rb
@@ -211,7 +211,8 @@ class Ia32
 					opsz = ia.sz
 				end
 			}
-			pfx << 0x66 if (opsz and size == 48 - opsz) or (op.props[:opsz] and op.props[:opsz] != size)
+			pfx << 0x66 if (not op.props[:argsz] or opsz != op.props[:argsz]) and (
+				(opsz and size == 48 - opsz) or (op.props[:opsz] and op.props[:opsz] != size))
 			if op.props[:opsz] and size == 48 - op.props[:opsz]
 				opsz = op.props[:opsz]
 			end
