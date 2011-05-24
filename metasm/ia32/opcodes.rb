49bb2a7703d1dfa618ba66e8e3cec0d5adbf6f2f
jj <none@none>
2011-05-24 14:34:02 +0200

    ia32: fix some sse instrs arg order

--- metasm/ia32/opcodes.rb
+++ metasm/ia32/opcodes.rb
@@ -481,8 +481,8 @@ class Ia32
 		addop('psadbw',  [0x0F, 0xF6], :mrmmmx) { |o| o.props[:xmmx] = true }
 		addop('pshufw',  [0x0F, 0x70], :mrmmmx) { |o| o.props[:xmmx] = true ; o.args << :u8 }
 		addop('maskmovq',[0x0F, 0xF7], :mrmmmx) { |o| o.props[:xmmx] = true } # nomem
-		addop('movntq',  [0x0F, 0xE7], :mrmmmx) { |o| o.props[:xmmx] = true }
-		addop 'movntps', [0x0F, 0x2B], :mrmxmm
+		addop('movntq',  [0x0F, 0xE7], :mrmmmx) { |o| o.args.reverse! ; o.props[:xmmx] = true }
+		addop('movntps', [0x0F, 0x2B], :mrmxmm) { |o| o.args.reverse! }
 		addop 'prefetcht0', [0x0F, 0x18, 1<<3], :modrmA
 		addop 'prefetcht1', [0x0F, 0x18, 2<<3], :modrmA
 		addop 'prefetcht2', [0x0F, 0x18, 3<<3], :modrmA
@@ -506,9 +506,9 @@ class Ia32
 		# nomem
 		addop('clflush', [0x0F, 0xAE, 7<<3], :modrmA) { |o| o.props[:argsz] = 8 }
 		addop('maskmovdqu', [0x0F, 0xF7], :mrmxmm) { |o| o.props[:needpfx] = 0x66 }
-		addop('movntpd', [0x0F, 0x2B], :mrmxmm) { |o| o.props[:needpfx] = 0x66 }
-		addop('movntdq', [0x0F, 0xE7], :mrmxmm) { |o| o.props[:needpfx] = 0x66 }
-		addop 'movnti',  [0x0F, 0xC3], :mrm
+		addop('movntpd', [0x0F, 0x2B], :mrmxmm) { |o| o.args.reverse! ; o.props[:needpfx] = 0x66 }
+		addop('movntdq', [0x0F, 0xE7], :mrmxmm) { |o| o.args.reverse! ; o.props[:needpfx] = 0x66 }
+		addop('movnti',  [0x0F, 0xC3], :mrm) { |o| o.args.reverse! }
 		addop('pause',   [0x90]) { |o| o.props[:needpfx] = 0xF3 }
 		addop 'lfence',  [0x0F, 0xAE, 0xE8]
 		addop 'mfence',  [0x0F, 0xAE, 0xF0]
