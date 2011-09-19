132d8d1f04f4fa7d99ac971cc27bc752fbc1476b
jj <none@none>
2011-09-19 15:34:00 +0200

    exe_fmt: add early .pyc file support

--- metasm.rb
+++ metasm.rb
@@ -42,6 +42,7 @@ module Metasm
 		'DEX' => 'exe_format/dex',
 		'NDS' => 'exe_format/nds', 'XCoff' => 'exe_format/xcoff',
 		'Bflt' => 'exe_format/bflt', 'Dol' => 'exe_format/dol',
+		'PYC' => 'exe_format/pyc',
 		'Gui' => 'gui',
 		'WindowsExports' => 'os/windows_exports',
 		'GNUExports' => 'os/gnu_exports',
