26479041862c7b801c23e97efa03e98bfa032c3e
jj <john-git@ofjj.net>
2014-05-27 00:13:40 +0200

    tests: rb2 encoding fixes

--- tests/ia32.rb
+++ tests/ia32.rb
@@ -16,55 +16,63 @@ class TestIa32 < Test::Unit::TestCase
 
 	def assert_equal(a, b) super(b, a) end
 
+	def bin(s)
+		if s.respond_to?(:force_encoding)
+			s.force_encoding('BINARY')
+		else
+			s
+		end
+	end
+
 	def test_basic
-		assert_equal(assemble("nop"), "\x90")
-		assert_equal(assemble("push eax"), "\x50")
-		assert_equal(assemble("push 2"), "\x6a\x02")
-		assert_equal(assemble("push 142"), "\x68\x8e\0\0\0")
+		assert_equal(assemble("nop"), bin("\x90"))
+		assert_equal(assemble("push eax"), bin("\x50"))
+		assert_equal(assemble("push 2"), bin("\x6a\x02"))
+		assert_equal(assemble("push 142"), bin("\x68\x8e\0\0\0"))
 	end
 
 	def test_sz
-		assert_equal(assemble("dec eax"), "\x48")
-		assert_equal(assemble("dec ax"), "\x66\x48")
-		assert_equal(assemble("dec al"), "\xfe\xc8")
+		assert_equal(assemble("dec eax"), bin("\x48"))
+		assert_equal(assemble("dec ax"), bin("\x66\x48"))
+		assert_equal(assemble("dec al"), bin("\xfe\xc8"))
 		assert_equal(assemble("arpl [edi+70h], bp"), "cop")
 	end
 
 	def test_16
-		assert_equal(assemble("push 142", @@cpu16), "\x68\x8e\0")
-		assert_equal(assemble("code16 push 142", @@cpu16), "\x68\x8e\0")
-		assert_equal(assemble("code16 push 142"), "\x68\x8e\0")
-		assert_equal(assemble("push.i16 142"), "\x66\x68\x8e\0")
-		assert_equal(assemble("mov eax, 42"), "\xb8\x2a\0\0\0")
-		assert_equal(assemble("code16 mov ax, 42"), "\xb8\x2a\0")
+		assert_equal(assemble("push 142", @@cpu16), bin("\x68\x8e\0"))
+		assert_equal(assemble("code16 push 142", @@cpu16), bin("\x68\x8e\0"))
+		assert_equal(assemble("code16 push 142"), bin("\x68\x8e\0"))
+		assert_equal(assemble("push.i16 142"), bin("\x66\x68\x8e\0"))
+		assert_equal(assemble("mov eax, 42"), bin("\xb8\x2a\0\0\0"))
+		assert_equal(assemble("code16 mov ax, 42"), bin("\xb8\x2a\0"))
 	end
 
 	def test_jmp
-		assert_equal(assemble("jmp $"), "\xeb\xfe")
-		assert_equal(assemble("jmp.i32 $"), "\xe9\xfb\xff\xff\xff")
+		assert_equal(assemble("jmp $"), bin("\xeb\xfe"))
+		assert_equal(assemble("jmp.i32 $"), bin("\xe9\xfb\xff\xff\xff"))
 	end
 
 	def test_opsz
-		assert_equal(assemble("cbw"), "\x66\x98")
-		assert_equal(assemble("cwde"), "\x98")
+		assert_equal(assemble("cbw"), bin("\x66\x98"))
+		assert_equal(assemble("cwde"), bin("\x98"))
 
-		assert_equal(assemble("cbw", @@cpu16), "\x98")
-		assert_equal(assemble("cwde", @@cpu16), "\x66\x98")
+		assert_equal(assemble("cbw", @@cpu16), bin("\x98"))
+		assert_equal(assemble("cwde", @@cpu16), bin("\x66\x98"))
 
-		assert_equal(assemble("cmpxchg8b [eax]"), "\x0f\xc7\x08")
-		assert_equal(assemble("cmpxchg8b [bx]", @@cpu16), "\x66\x0f\xc7\x0f")
+		assert_equal(assemble("cmpxchg8b [eax]"), bin("\x0f\xc7\x08"))
+		assert_equal(assemble("cmpxchg8b [bx]", @@cpu16), bin("\x66\x0f\xc7\x0f"))
 	end
 
 	def test_mrmsz
-		assert_equal(assemble("mov [eax], ebx"), "\x89\x18")
-		assert_equal(assemble("mov [eax], bl"), "\x88\x18")
-		assert_equal(assemble("mov ebx, [eax]"), "\x8b\x18")
-		assert_equal(assemble("mov bl, [eax]"), "\x8a\x18")
-		assert_equal(assemble("mov bl, [bx]"), "\x67\x8a\x1f")
-		assert_equal(assemble("mov bl, [bx]", @@cpu16), "\x8a\x1f")
-		assert_equal(assemble("code16 mov bl, [bx]"), "\x8a\x1f")
-		assert_equal(assemble("mov bl, [0]"), "\x8a\x1d\0\0\0\0")
-		assert_equal(assemble("mov.a16 bl, [0]"), "\x67\x8a\x1e\0\0")
+		assert_equal(assemble("mov [eax], ebx"), bin("\x89\x18"))
+		assert_equal(assemble("mov [eax], bl"), bin("\x88\x18"))
+		assert_equal(assemble("mov ebx, [eax]"), bin("\x8b\x18"))
+		assert_equal(assemble("mov bl, [eax]"), bin("\x8a\x18"))
+		assert_equal(assemble("mov bl, [bx]"), bin("\x67\x8a\x1f"))
+		assert_equal(assemble("mov bl, [bx]", @@cpu16), bin("\x8a\x1f"))
+		assert_equal(assemble("code16 mov bl, [bx]"), bin("\x8a\x1f"))
+		assert_equal(assemble("mov bl, [0]"), bin("\x8a\x1d\0\0\0\0"))
+		assert_equal(assemble("mov.a16 bl, [0]"), bin("\x67\x8a\x1e\0\0"))
 	end
 
 	def test_err
@@ -85,26 +93,26 @@ class TestIa32 < Test::Unit::TestCase
 	end
 
 	def test_dasm
-		d = disassemble("\x90")
+		d = disassemble(bin("\x90"))
 		assert_equal(d.decoded[0].class, Metasm::DecodedInstruction)
 		assert_equal(d.decoded[0].opcode.name, "nop")
 
-		assert_equal(disassemble("\x66\x0f\xc7\x08").decoded[0], nil)
-		assert_equal(disassemble("\x0f\xc7\x08").decoded[0].opcode.name, "cmpxchg8b")
+		assert_equal(disassemble(bin("\x66\x0f\xc7\x08")).decoded[0], nil)
+		assert_equal(disassemble(bin("\x0f\xc7\x08")).decoded[0].opcode.name, "cmpxchg8b")
 	end
 
 	def test_pfx
-		assert_equal(assemble("nop"), "\x90")
-		assert_equal(assemble("pause"), "\xf3\x90")
-		assert_equal(disassemble("\x90").decoded.values.first.opcode.name, "nop")
-		assert_equal(disassemble("\xf3\x90").decoded.values.first.opcode.name, "pause")
+		assert_equal(assemble("nop"), bin("\x90"))
+		assert_equal(assemble("pause"), bin("\xf3\x90"))
+		assert_equal(disassemble(bin("\x90")).decoded.values.first.opcode.name, "nop")
+		assert_equal(disassemble(bin("\xf3\x90")).decoded.values.first.opcode.name, "pause")
 	end
 
 	def test_avx
-		assert_equal(disassemble("\xc4\xc3\x75\x42\xc2\x03").decoded[0].instruction.to_s, "vmpsadbw ymm0, ymm1, ymm2, 3")
-		assert_equal(assemble("vmpsadbw ymm0, ymm1, ymm2, 3"), "\xc4\xc3\x75\x42\xc2\x03")
-		assert_equal(assemble("vpblendvb xmm1, xmm2, xmm3, xmm4"), "\xc4\xc3\x69\x4c\xcb\x40")
-		assert_equal(assemble("vgatherdpd xmm1, qword ptr [edx+xmm1], xmm2"), "\xc4\xc2\xe9\x92\x0c\x0a")
-		assert_equal(disassemble("\xc4\xc2\xe9\x92\x0c\x0a").decoded[0].instruction.to_s, "vgatherdpd xmm1, qword ptr [edx+xmm1], xmm2")
+		assert_equal(disassemble(bin("\xc4\xc3\x75\x42\xc2\x03")).decoded[0].instruction.to_s, "vmpsadbw ymm0, ymm1, ymm2, 3")
+		assert_equal(assemble("vmpsadbw ymm0, ymm1, ymm2, 3"), bin("\xc4\xc3\x75\x42\xc2\x03"))
+		assert_equal(assemble("vpblendvb xmm1, xmm2, xmm3, xmm4"), bin("\xc4\xc3\x69\x4c\xcb\x40"))
+		assert_equal(assemble("vgatherdpd xmm1, qword ptr [edx+xmm1], xmm2"), bin("\xc4\xc2\xe9\x92\x0c\x0a"))
+		assert_equal(disassemble(bin("\xc4\xc2\xe9\x92\x0c\x0a")).decoded[0].instruction.to_s, "vgatherdpd xmm1, qword ptr [edx+xmm1], xmm2")
 	end
 end
--- tests/x86_64.rb
+++ tests/x86_64.rb
@@ -17,16 +17,24 @@ class TestX86_64 < Test::Unit::TestCase
 		assert_equal(Metasm::X86_64, Metasm::Ia32.new(64).class)
 	end
 
+	def bin(s)
+		if s.respond_to?(:force_encoding)
+			s.force_encoding('BINARY')
+		else
+			s
+		end
+	end
+
 	def test_basic
-		assert_equal("\x90", assemble("nop"))
-		assert_equal("\x50", assemble("push rax"))
-		assert_equal("\x41\x50", assemble("push r8"))
-		assert_equal("\x6a\x02", assemble("push 2"))
-		assert_equal("\x68\x8e\0\0\0", assemble("push 142"))
-		assert_equal("\x48\xbb\xef\xcd\xab\x89\x67\x45\x23\x01", assemble("mov rbx, 0123456789abcdefh"))
-		assert_equal("\x8d\x05\x0c\0\0\0", assemble("lea eax, [rip+12]"))
-		assert_equal("\x8d\x04\x25\x0c\0\0\0", assemble("lea eax, [12]"))
-		assert_equal("\x48\x81\xE3\xFF\xF0\xFF\xFF", assemble("and rbx, 0xffffffff_fffff0ff"))
+		assert_equal(bin("\x90"), assemble("nop"))
+		assert_equal(bin("\x50"), assemble("push rax"))
+		assert_equal(bin("\x41\x50"), assemble("push r8"))
+		assert_equal(bin("\x6a\x02"), assemble("push 2"))
+		assert_equal(bin("\x68\x8e\0\0\0"), assemble("push 142"))
+		assert_equal(bin("\x48\xbb\xef\xcd\xab\x89\x67\x45\x23\x01"), assemble("mov rbx, 0123456789abcdefh"))
+		assert_equal(bin("\x8d\x05\x0c\0\0\0"), assemble("lea eax, [rip+12]"))
+		assert_equal(bin("\x8d\x04\x25\x0c\0\0\0"), assemble("lea eax, [12]"))
+		assert_equal(bin("\x48\x81\xE3\xFF\xF0\xFF\xFF"), assemble("and rbx, 0xffffffff_fffff0ff"))
 	end
 
 	def test_err
@@ -45,52 +53,52 @@ class TestX86_64 < Test::Unit::TestCase
 	end
 
 	def test_dasm
-		d = disassemble("\x90")
+		d = disassemble(bin("\x90"))
 		assert_equal(Metasm::DecodedInstruction, d.decoded[0].class)
 		assert_equal('nop', d.decoded[0].opcode.name)
 	end
 
 	def test_rex
-		assert_equal("\xfe\xc0", assemble("inc al"))
-		assert_equal("\xfe\xc4", assemble("inc ah"))
-		assert_equal("\x40\xfe\xc4", assemble("inc spl"))
-		assert_equal("\x41\xfe\xc4", assemble("inc r12b"))
+		assert_equal(bin("\xfe\xc0"), assemble("inc al"))
+		assert_equal(bin("\xfe\xc4"), assemble("inc ah"))
+		assert_equal(bin("\x40\xfe\xc4"), assemble("inc spl"))
+		assert_equal(bin("\x41\xfe\xc4"), assemble("inc r12b"))
 		op = lambda { |s| i = disassemble(s).decoded[0].instruction ; i.to_s ; i.args.last.to_s }
-		assert_equal('al', op["\xfe\xc0"])
-		assert_equal('ah', op["\xfe\xc4"])
-		assert_equal('spl', op["\x40\xfe\xc4"])
-		assert_equal('r12b', op["\x41\xfe\xc4"])
-		assert_equal('[rip-6+12h]', op["\x8d\x05\x0c\0\0\0"])
+		assert_equal('al', op[bin("\xfe\xc0")])
+		assert_equal('ah', op[bin("\xfe\xc4")])
+		assert_equal('spl', op[bin("\x40\xfe\xc4")])
+		assert_equal('r12b', op[bin("\x41\xfe\xc4")])
+		assert_equal('[rip-6+12h]', op[bin("\x8d\x05\x0c\0\0\0")])
 	end
 
 	def test_opsz
-		assert_equal("\x66\x98", assemble("cbw"))
-		assert_equal("\x98", assemble("cwde"))
-		assert_equal("\x48\x98", assemble("cdqe"))
+		assert_equal(bin("\x66\x98"), assemble("cbw"))
+		assert_equal(bin("\x98"), assemble("cwde"))
+		assert_equal(bin("\x48\x98"), assemble("cdqe"))
 
-		assert_equal("\x0f\xc7\x08", assemble("cmpxchg8b [rax]"))
-		assert_equal("\x48\x0f\xc7\x08", assemble("cmpxchg16b [rax]"))
+		assert_equal(bin("\x0f\xc7\x08"), assemble("cmpxchg8b [rax]"))
+		assert_equal(bin("\x48\x0f\xc7\x08"), assemble("cmpxchg16b [rax]"))
 
-		assert_equal(nil, disassemble("\x66\x0f\xc7\x08").decoded[0])
-		assert_equal('cmpxchg8b', disassemble("\x47\x0f\xc7\x08").decoded[0].opcode.name)
-		assert_equal('cmpxchg16b', disassemble("\x48\x0f\xc7\x08").decoded[0].opcode.name)
+		assert_equal(nil, disassemble(bin("\x66\x0f\xc7\x08")).decoded[0])
+		assert_equal('cmpxchg8b', disassemble(bin("\x47\x0f\xc7\x08")).decoded[0].opcode.name)
+		assert_equal('cmpxchg16b', disassemble(bin("\x48\x0f\xc7\x08")).decoded[0].opcode.name)
 	end
 
 	def test_avx
-		assert_equal('vmpsadbw ymm12, ymm14, ymm2, 3', disassemble("\xc4\x63\x0d\x42\xe2\x03").decoded[0].instruction.to_s)
-		assert_equal("\xc4\x63\x0d\x42\xe2\x03", assemble('vmpsadbw ymm12, ymm14, ymm2, 3'))
-		assert_equal("\xc5\x31\x63\xc2", assemble('vpacksswb xmm8, xmm9, xmm2'))
-		assert_equal("\xc4\x41\x31\x63\xc2", assemble('vpacksswb xmm8, xmm9, xmm10'))
-		assert_equal("\xc5\x31\x63\x04\x5a", assemble('vpacksswb xmm8, xmm9, [rdx+2*rbx]'))
-		assert_equal("\xc4\x01\x31\x63\x04\x5a", assemble('vpacksswb xmm8, xmm9, [r10+2*r11]'))
-		assert_equal("\xc4\x22\x99\x92\x14\x1a", assemble('vgatherdpd xmm10, qword ptr [rdx+xmm11], xmm12'))
-		assert_equal('vgatherdpd xmm10, qword ptr [rdx+xmm11], xmm12', disassemble("\xc4\x22\x99\x92\x14\x1a").decoded[0].instruction.to_s)
+		assert_equal('vmpsadbw ymm12, ymm14, ymm2, 3', disassemble(bin("\xc4\x63\x0d\x42\xe2\x03")).decoded[0].instruction.to_s)
+		assert_equal(bin("\xc4\x63\x0d\x42\xe2\x03"), assemble('vmpsadbw ymm12, ymm14, ymm2, 3'))
+		assert_equal(bin("\xc5\x31\x63\xc2"), assemble('vpacksswb xmm8, xmm9, xmm2'))
+		assert_equal(bin("\xc4\x41\x31\x63\xc2"), assemble('vpacksswb xmm8, xmm9, xmm10'))
+		assert_equal(bin("\xc5\x31\x63\x04\x5a"), assemble('vpacksswb xmm8, xmm9, [rdx+2*rbx]'))
+		assert_equal(bin("\xc4\x01\x31\x63\x04\x5a"), assemble('vpacksswb xmm8, xmm9, [r10+2*r11]'))
+		assert_equal(bin("\xc4\x22\x99\x92\x14\x1a"), assemble('vgatherdpd xmm10, qword ptr [rdx+xmm11], xmm12'))
+		assert_equal('vgatherdpd xmm10, qword ptr [rdx+xmm11], xmm12', disassemble(bin("\xc4\x22\x99\x92\x14\x1a")).decoded[0].instruction.to_s)
 	end
 
 	def test_lol
 		# x64 nop weirdnesses
-		assert_equal("\x87\xc0", assemble('xchg eax, eax'))
-		assert_equal('xchg r8, rax', disassemble("\x49\x90").decoded[0].instruction.to_s)
+		assert_equal(bin("\x87\xc0"), assemble('xchg eax, eax'))
+		assert_equal('xchg r8, rax', disassemble(bin("\x49\x90")).decoded[0].instruction.to_s)
 	end
 
 	def test_C_size
