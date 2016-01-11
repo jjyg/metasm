236ab2f59db9e00db0a573520e216be57bd2bcb3
jj <john-git@ofjj.net>
2016-01-11 16:24:58 +0100

    elf_encode: fix some rb encoding nonsense

--- metasm/exe_format/elf.rb
+++ metasm/exe_format/elf.rb
@@ -9,6 +9,7 @@ require 'metasm/exe_format/main'
 module Metasm
 class ELF < ExeFormat
 	MAGIC = "\x7fELF"	# 0x7f454c46
+	MAGIC.force_encoding('BINARY') if MAGIC.respond_to?(:force_encoding)
 	CLASS = { 0 => 'NONE', 1 => '32', 2 => '64', 200 => '64_icc' }
 	DATA  = { 0 => 'NONE', 1 => 'LSB', 2 => 'MSB' }
 	VERSION = { 0 => 'INVALID', 1 => 'CURRENT' }
@@ -395,6 +396,8 @@ class ELF < ExeFormat
 
 	DEFAULT_INTERP = '/lib/ld-linux.so.2'
 	DEFAULT_INTERP64 = '/lib64/ld-linux-x86-64.so.2'
+	DEFAULT_INTERP.force_encoding('BINARY') if DEFAULT_INTERP.respond_to?(:force_encoding)
+	DEFAULT_INTERP64.force_encoding('BINARY') if DEFAULT_INTERP64.respond_to?(:force_encoding)
 
 	class SerialStruct < Metasm::SerialStruct
 		new_int_field :addr, :off, :xword, :sword, :sxword
@@ -682,6 +685,7 @@ end
 
 class FatELF < ExeFormat
 	MAGIC = "\xfa\x70\x0e\x1f"	# 0xfat..elf
+	MAGIC.force_encoding('BINARY') if MAGIC.respond_to?(:force_encoding)
 
 	class SerialStruct < Metasm::SerialStruct
 		new_int_field :qword
