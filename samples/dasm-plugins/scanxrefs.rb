3027c2f7f46088c08ba01512c556f3dbef26f00d
jj <john-git@ofjj.net>
2016-09-02 15:10:02 +0200

    dasm-plugins/scanxrefs: fix x64

--- samples/dasm-plugins/scanxrefs.rb
+++ samples/dasm-plugins/scanxrefs.rb
@@ -7,11 +7,12 @@
 # metasm dasm plugin: scan for xrefs to the target address, incl. relative offsets (eg near call/jmp)
 def scanxrefs(target)
 	ans = []
+	msk = (1 << cpu.size) - 1
 	sections.sort.each { |s_addr, edata|
 		raw = edata.data.to_str
 		(0..raw.length-4).each { |off|
 			r = raw[off, 4].unpack('V').first
-			ans << (s_addr + off) if (r + off+4 + s_addr)&0xffffffff == target or r == target
+			ans << (s_addr + off) if (r + off+4 + s_addr) & msk == target or r == target
 		}
 	}
 	ans
