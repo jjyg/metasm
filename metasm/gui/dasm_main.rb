2ffc85dad739cfc35841e7dfdc2eb31be3876e08
jj <john-git@ofjj.net>
2018-06-28 12:06:52 +0200

    gui/dasm: add option to spawn emudbg

--- metasm/gui/dasm_main.rb
+++ metasm/gui/dasm_main.rb
@@ -853,6 +853,12 @@ class DisasmWidget < ContainerChoiceWidget
 		end
 	end
 
+	def spawn_emudbg
+		edbg = EmuDebugger.new(@dasm)
+		edbg.pc = curaddr
+		DbgWindow.new(edbg)
+	end
+
 	def extend_contextmenu(tg, menu, addr=nil)
 		if @parent_widget.respond_to?(:extend_contextmenu)
 			@parent_widget.extend_contextmenu(tg, menu, addr)
@@ -1124,6 +1130,7 @@ class DasmWindow < Window
 		addsubmenu(actions, 'Undefine function & _subfuncs') { @dasm_widget.undefine_function(@dasm_widget.curview.current_address, true) }
 		addsubmenu(actions, 'Data', 'd') { @dasm_widget.toggle_data(@dasm_widget.curview.current_address) }
 		addsubmenu(actions, 'Pause dasm', 'p', :check) { |ck| !@dasm_widget.playpause_dasm }
+		addsubmenu(actions, 'Spawn EbuDb_g', 'G') { @dasm_widget.spawn_emudbg }
 		addsubmenu(actions, 'Run ruby snippet', '^r') { promptruby }
 		addsubmenu(actions, 'Run _ruby plugin') { @dasm_widget.prompt_run_ruby_plugin }
 
