5d5a72769d37fedea7a0cfd9eca950cb64a9c73b
jj <john-git@ofjj.net>
2019-04-24 18:48:33 +0200

    cpu/dwarf: tweak decompile

--- metasm/cpu/dwarf/decompile.rb
+++ metasm/cpu/dwarf/decompile.rb
@@ -10,21 +10,34 @@ module Metasm
 class Dwarf
 	def decompile_makestackvars(dasm, funcstart, blocks)
 		oldbd = {}
-		oldbd[funcstart] = dasm.address_binding[funcstart]
-		dasm.address_binding[funcstart] = { :opstack => Expression[:frameptr] }
 		blocks.each { |block|
 			oldbd[block.address] = dasm.address_binding[block.address]
-			stkoff = dasm.backtrace(:opstack, block.address, :snapshot_addr => funcstart)
-			dasm.address_binding[block.address] = { :opstack => Expression[:frameptr, :+, stkoff[0]-:frameptr] }
-			yield block
-			if di = block.list.last and di.opcode.name == 'bra'
-				# remember the stack offset of the stack value to be checked
-				stkoff = dasm.backtrace(:opstack, di.address, :snapshot_addr => funcstart)
-				if stkoff.length == 1 and (stkoff[0] - :frameptr).kind_of?(::Integer)
-					di.misc ||= {}
-					di.misc[:dcmp_stackoff] = stkoff[0] - :frameptr
-				end
+			dasm.address_binding[block.address] = {}
+                }
+
+		dasm.address_binding[funcstart][:opstack] = Expression[:frameptr]
+		blocks.each { |block|
+			# cache the value of opstack wrt :frameptr at each block entry
+			if not stkoff = dasm.address_binding[block.address][:opstack]
+				stkoff = dasm.backtrace(:opstack, block.address, :snapshot_addr => funcstart)
+				# conserve the minimum offset in case of conflicts
+				stkoff = Expression[:frameptr] + stkoff.map { |so| so - :frameptr }.min
 			end
+			dasm.address_binding[block.address][:opstack] = stkoff
+			block.list.first.misc ||= {}
+			block.list.first.misc[:opstack_before] = stkoff
+
+			# compute the value at the end of the block and propagate as start value for next blocks
+			# allows coherent tracing along all paths if blocks are walked in code order, even with loops with stack leak/consume
+			last_di = block.list.last
+			stkoff = dasm.backtrace(:opstack, last_di.address, :snapshot_addr => funcstart, :include_start => true).first
+			last_di.misc ||= {}
+			last_di.misc[:opstack_after] = stkoff
+			block.each_to_normal { |at|
+				dasm.address_binding[at][:opstack] ||= stkoff if dasm.address_binding[at]
+			}
+
+			yield block
 		}
 		oldbd.each { |a, b| b ? dasm.address_binding[a] = b : dasm.address_binding.delete(a) }
 	end
@@ -40,8 +53,9 @@ class Dwarf
 		scope = func.initializer
 		stmts = scope.statements
 
+		# TODO handle loops pushing/poping the stack (:opstack_before loop.first != :opstack_after loop.last)
+
 		# opstack offset => current C variable
-		# TODO per-basicblock to handle crossreferences in loops / merges
 		opstack = {}
 
 		# *(_int32*)(local_base+16) => 16
@@ -60,16 +74,18 @@ class Dwarf
 				end
 			end
 		}
-		opstack_idx = -1
 		ce_opstack_offset = lambda { |ee| ce_ptr_offset[ee, 'frameptr'] }
 
 		basetype = C::BaseType.new("__int#@size".to_sym)
 		new_opstack_var = lambda { |off|
-			varname = "loc_#{opstack_idx += 1}"
+			varname = "loc_#{off}"
 			ne = C::Variable.new(varname, basetype)
 			scope.symbol[varname] = ne
 			stmts << C::Declaration.new(ne)
-			opstack[off] = ne
+			ne
+		}
+		get_opstack_var = lambda { |off|
+			opstack[off] ||= new_opstack_var[off]
 		}
 
 		di_addr = nil
@@ -80,22 +96,18 @@ class Dwarf
 			dcmp.walk_ce(c_expr, true) { |ee|
 				if soff = ce_opstack_offset[ee.rexpr]
 					# must do soff.rexpr before lexpr in case of reaffectation !
-					ee.rexpr = opstack[soff/8] if opstack[soff/8]
+					ee.rexpr = get_opstack_var[soff/8]
 					ee.rexpr = C::CExpression[ee.rexpr] if not ee.op and ee.type.pointer?
 				end
 				if soff = ce_opstack_offset[ee.lexpr]
-					if ee.op == :'='
-						# affectation: create a new variable
-						new_opstack_var[soff/8]
-					end
-					ee.lexpr = opstack[soff/8] if opstack[soff/8]
+					ee.lexpr = get_opstack_var[soff/8]
 				end
 			}
-			ret = if soff = ce_opstack_offset[c_expr] and opstack[soff/8]
-				C::CExpression[opstack[soff/8]]
-			else
-				c_expr
-			end
+			ret = if soff = ce_opstack_offset[c_expr]
+					 C::CExpression[get_opstack_var[soff/8]]
+				else
+					c_expr
+				end
 			dcmp.walk_ce(ret) { |ee| ee.with_misc :di_addr => di_addr } if di_addr
 			ret
 		}
@@ -105,37 +117,38 @@ class Dwarf
 		until myblocks.empty?
 			b, to = myblocks.shift
 			if l = dcmp.dasm.get_label_at(b)
-				stmts << C::Label.new(l)
+				stmts << C::Label.new(l).with_misc(:di_addr => b)
 			end
 
 			# go !
 			di_list = dcmp.dasm.decoded[b].block.list.dup
 			di_list.each { |di|
 				di_addr = di.address
+				bd = get_fwdemu_binding(di)
 				case di.opcode.name
 				when 'bra'
 					n = dcmp.backtrace_target(get_xrefs_x(dcmp.dasm, di).first, di.address)
-					off = di.misc[:dcmp_stackoff] || -8
-					cc = ce[Indirection[[:frameptr, :+, off], @size/8]]
-					stmts << C::If.new(C::CExpression[cc], C::Goto.new(n).with_misc(:di_addr => di.address)).with_misc(:di_addr => di.address)
+					stacktop = ce[get_opstack_var[(bd[:opstack] - :frameptr + 8) / 8]]
+					stmts << C::If.new(C::CExpression[stacktop], C::Goto.new(n).with_misc(:di_addr => di.address)).with_misc(:di_addr => di.address)
+					stmts << ce[stacktop, :'=', 0]
+					# XXX does not assign cond = 0 in if as decompiler may expect only single goto in if body
 					to.delete dcmp.dasm.normalize(n)
 				when 'swap'
-					bd = get_fwdemu_binding(di)
 					offs = []
 					bd.each { |k, v|
-						next if k == :opstack
 						cvar = dcmp.decompile_cexpr(Expression[Expression[k].reduce], scope)
 						if soff = ce_opstack_offset[cvar]
 							offs << (soff/8)
 						end
 					}
 					off = offs.min
-					opstack[off], opstack[off+1] = opstack[off+1], opstack[off]
+					stmts << C::CExpression[get_opstack_var[:tmp], :'=', get_opstack_var[off]]
+					stmts << C::CExpression[get_opstack_var[off], :'=', get_opstack_var[off+1]]
+					stmts << C::CExpression[get_opstack_var[off+1], :'=', get_opstack_var[:tmp]]
+					stmts << C::CExpression[get_opstack_var[:tmp],  :'=', 0]
 				when 'rot'
-					bd = get_fwdemu_binding(di)
 					offs = []
 					bd.each { |k, v|
-						next if k == :opstack
 						cvar = dcmp.decompile_cexpr(Expression[Expression[k].reduce], scope)
 						if soff = ce_opstack_offset[cvar]
 							offs << (soff/8)
@@ -143,9 +156,12 @@ class Dwarf
 					}
 
 					off = offs.min
-					opstack[off], opstack[off+1], opstack[off+2] = opstack[off+2], opstack[off], opstack[off+1]
+					stmts << C::CExpression[get_opstack_var[:tmp],  :'=', get_opstack_var[off]]
+					stmts << C::CExpression[get_opstack_var[off],   :'=', get_opstack_var[off+2]]
+					stmts << C::CExpression[get_opstack_var[off+2], :'=', get_opstack_var[off+1]]
+					stmts << C::CExpression[get_opstack_var[off+1], :'=', get_opstack_var[:tmp]]
+					stmts << C::CExpression[get_opstack_var[:tmp],  :'=', 0]
 				else
-					bd = get_fwdemu_binding(di)
 					if di.backtrace_binding[:incomplete_binding]
 						stmts << C::Asm.new(di.instruction.to_s, nil, nil, nil, nil, nil).with_misc(:di_addr => di.address)
 					else
@@ -154,6 +170,11 @@ class Dwarf
 							e = ce[k, :'=', v]
 							stmts << e if not e.kind_of?(C::Variable)	# [:eflag_s, :=, :unknown].reduce
 						}
+						rawbd = dcmp.disassembler.cpu.get_backtrace_binding(di)
+						if rawbd[:opstack] == Expression[:opstack, :-, 8] and (bd[:opstack] - :frameptr).kind_of?(::Integer)
+							stacktop = ce[get_opstack_var[(bd[:opstack] - :frameptr + 8) / 8]]
+							stmts << ce[stacktop, :'=', 0]
+						end
 					end
 				end
 				di_addr = nil
@@ -166,7 +187,11 @@ class Dwarf
 				end
 			when 1
 				if (myblocks.empty? ? nextaddr != to[0] : myblocks.first.first != to[0])
-					stmts << C::Goto.new(dcmp.dasm.auto_label_at(to[0], 'unknown_goto'))
+					if dcmp.dasm.decoded[to[0]]
+						stmts << C::Goto.new(dcmp.dasm.auto_label_at(to[0], 'unknown_goto'))
+					else
+						stmts << C::Return.new(ce[get_opstack_var[(di_list.last.misc[:opstack_after] - :frameptr)/8]])
+					end
 				end
 			else
 				puts "  block #{Expression[b]} with multiple to"
